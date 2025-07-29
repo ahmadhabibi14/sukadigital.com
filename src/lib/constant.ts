b2c8d1d9001af1e77cf7c92e81977e9d8606ac69
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-07-29 13:06:52 +0800

    [fix] site desc

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,5 @@
 export const SiteName: string = 'Suka Digital';
-export const SiteDesc: string = 'Suka Digital';
+export const SiteDesc: string = 'Layanan Konsultasi, aktivasi dan instalasi Jaringan Internet of Things';
 export const SiteBaseUrl: string = 'https://sukadigital.vercel.app';
 export const SiteImageUrl: string = 'https://sukadigital.vercel.app/img/opengraph.png';
 export const SiteAuthor: string = 'Suka Digital';
